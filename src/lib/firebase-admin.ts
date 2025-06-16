5207c42d6e2cbded1e04fc712086b1bd1c46a141
johnekenetech <johnekenetech@gmail.com>
2025-06-16 17:45:20 +0100

    deployment fix

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,16 +1,15 @@
 import admin from "firebase-admin";
-import serviceAccount from '../../config/serviceAccountKey.json';
+
 
 if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Replace escaped newlines in private key (from .env) with real newlines
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
-    });
-  }
-  
-  export const adminDb = admin.firestore();
-  export { admin };
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
+const adminDb = admin.firestore();
+export { adminDb, admin };
